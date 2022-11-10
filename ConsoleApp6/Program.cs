28880cb3f4841a7e04060f56acd7232871ef707a
gvanca76 <gvanca.eliauri76@gmail.com>
2022-11-10 15:05:03 +0400

    first visual studio push

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.Write("bb");
+            Console.WriteLine("ccccccc");
+            Console.WriteLine("ddddddddd");
+
+        }
+    }
+}
